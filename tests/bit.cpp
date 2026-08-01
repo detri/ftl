0ea63bc6100f57efaca07bb280d7c68d7de7b02a
Aaron Dosser <adosser@ded.zone>
2026-08-01 15:36:13 -0400

    Bit conformance

--- tests/bit.cpp
+++ tests/bit.cpp
@@ -11,6 +11,7 @@ namespace tested = ftl;
 static_assert(tested::bit_cast<tested::uint32_t>(1.0f) == 0x3f800000u);
 static_assert(tested::byteswap(tested::uint32_t{0x01020304}) == 0x04030201);
 static_assert(tested::byteswap(-2) < 0);
+static_assert(tested::byteswap('x') == 'x');
 static_assert(tested::rotl(tested::uint8_t{1}, 1) == 2);
 static_assert(tested::popcount(0xf0u) == 4);
 static_assert(tested::bit_ceil(5u) == 8);
