af8b625c8db0ef84feb7a6454e784915fb94da86
Aaron Dosser <adosser@ded.zone>
2026-08-05 19:43:46 -0400

    Extents pt2

--- tests/mdspan.cpp
+++ tests/mdspan.cpp
@@ -140,4 +140,106 @@ constexpr bool mdspan_extents_works() {
 
 static_assert(mdspan_extents_works());
 
-bool ftl_test() { return mdspan_extents_works(); }
+using static_2x3 = tested::extents<int, 2, 3>;
+using static_2x4 = tested::extents<int, 2, 4>;
+using dynamic_2d =
+    tested::extents<int, tested::dynamic_extent, tested::dynamic_extent>;
+
+using wide_dynamic =
+    tested::extents<long long, tested::dynamic_extent>;
+using narrow_dynamic =
+    tested::extents<int, tested::dynamic_extent>;
+
+static_assert(tested::is_convertible_v<static_2x3, dynamic_2d>);
+static_assert(tested::is_constructible_v<static_2x3, dynamic_2d>);
+static_assert(!tested::is_convertible_v<dynamic_2d, static_2x3>);
+
+static_assert(!tested::is_constructible_v<static_2x4, static_2x3>);
+
+static_assert(tested::is_convertible_v<narrow_dynamic, wide_dynamic>);
+static_assert(tested::is_constructible_v<narrow_dynamic, wide_dynamic>);
+static_assert(!tested::is_convertible_v<wide_dynamic, narrow_dynamic>);
+
+static_assert(tested::is_constructible_v<
+              mixed_extents, tested::array<int, 2>>);
+static_assert(tested::is_convertible_v<
+              tested::array<int, 2>, mixed_extents>);
+
+static_assert(tested::is_constructible_v<
+              mixed_extents, tested::array<int, 4>>);
+static_assert(!tested::is_convertible_v<
+              tested::array<int, 4>, mixed_extents>);
+
+static_assert(tested::is_constructible_v<
+              mixed_extents, tested::span<int, 2>>);
+static_assert(tested::is_convertible_v<
+              tested::span<int, 2>, mixed_extents>);
+
+static_assert(tested::is_constructible_v<
+              mixed_extents, tested::span<int, 4>>);
+static_assert(!tested::is_convertible_v<
+              tested::span<int, 4>, mixed_extents>);
+
+constexpr bool mdspan_extents_conversions_work() {
+    static_2x3 fixed;
+    dynamic_2d dynamic = fixed;
+
+    if (dynamic.extent(0) != 2 || dynamic.extent(1) != 3)
+        return false;
+
+    dynamic_2d matching{2, 3};
+    static_2x3 restored{matching};
+
+    if (restored.extent(0) != 2 || restored.extent(1) != 3)
+        return false;
+
+    tested::array<int, 2> compact_values{5, 7};
+    mixed_extents from_compact_array = compact_values;
+
+    if (from_compact_array.extent(0) != 2 ||
+        from_compact_array.extent(1) != 5 ||
+        from_compact_array.extent(2) != 4 ||
+        from_compact_array.extent(3) != 7)
+        return false;
+
+    tested::array<int, 4> complete_values{2, 11, 4, 13};
+    mixed_extents from_complete_array{complete_values};
+
+    if (from_complete_array.extent(0) != 2 ||
+        from_complete_array.extent(1) != 11 ||
+        from_complete_array.extent(2) != 4 ||
+        from_complete_array.extent(3) != 13)
+        return false;
+
+    int compact_data[] = {17, 19};
+    tested::span compact_span{compact_data};
+    mixed_extents from_compact_span = compact_span;
+
+    if (from_compact_span.extent(1) != 17 ||
+        from_compact_span.extent(3) != 19)
+        return false;
+
+    int complete_data[] = {2, 23, 4, 29};
+    tested::span complete_span{complete_data};
+    mixed_extents from_complete_span{complete_span};
+
+    if (from_complete_span.extent(1) != 23 ||
+        from_complete_span.extent(3) != 29)
+        return false;
+
+    if (!(fixed == dynamic))
+        return false;
+
+    if (fixed == static_2x4{})
+        return false;
+
+    tested::extents<int, 2> rank_one;
+    if (fixed == rank_one)
+        return false;
+
+    return true;
+}
+
+static_assert(mdspan_extents_conversions_work());
+
+bool ftl_test() { return mdspan_extents_works() && mdspan_extents_conversions_work(); }
