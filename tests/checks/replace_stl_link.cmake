66d065c0de40c73fc4b2f3eeccfde42214533671
Aaron Dosser <adosser@ded.zone>
2026-08-24 15:30:15 -0400

    fix: bad regex when checking freestanding linkage for Apple

--- tests/checks/replace_stl_link.cmake
+++ tests/checks/replace_stl_link.cmake
@@ -9,6 +9,6 @@ if (NOT link_map MATCHES "ftl_compiler_runtime")
     message(FATAL_ERROR "FTL compiler runtime is absent from the linker map")
 endif ()
 
-if (link_map MATCHES "libstdc\\+\\+|libc\\+\\+|libcpmt|msvcprt")
+if (link_map MATCHES "libstdc\\+\\+|libc\\+\\+[.]|libcpmt|msvcprt")
     message(FATAL_ERROR "vendor C++ standard library found in the linker map")
 endif ()
